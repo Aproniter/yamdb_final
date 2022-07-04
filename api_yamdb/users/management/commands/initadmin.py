aa6e3df651ed4a15ab6eaf7b155215713a5af1dd
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 14:27:50 +0300

    fix initadmin

--- api_yamdb/users/management/commands/initadmin.py
+++ api_yamdb/users/management/commands/initadmin.py
@@ -11,9 +11,9 @@ class Command(BaseCommand):
             username = os.getenv('DJANGO_SUPERUSER_USERNAME')
             email = os.getenv('DJANGO_SUPERUSER_EMAIL')
             password = os.getenv('DJANGO_SUPERUSER_PASSWORD')            
-            admin = Account.objects.create_superuser(email=email, username=username, password=password)
+            admin = User.objects.create_superuser(email=email, username=username, password=password)
             admin.is_active = True
             admin.is_admin = True
             admin.save()
         else:
-            print('Admin accounts can only be initialized if no Accounts exist')
+            print('Admin accounts can only be initialized if no Users exist')
