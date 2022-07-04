4dba4f0a82ed820e03baec14db73ae897833029c
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 14:32:40 +0300

    fix initadmin

--- api_yamdb/users/management/commands/initadmin.py
+++ api_yamdb/users/management/commands/initadmin.py
@@ -1,19 +1,26 @@
 import os
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from users.models import User
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if User.objects.count() == 0:           
+        if User.objects.count() == 0:
             username = os.getenv('DJANGO_SUPERUSER_USERNAME')
             email = os.getenv('DJANGO_SUPERUSER_EMAIL')
-            password = os.getenv('DJANGO_SUPERUSER_PASSWORD')            
-            admin = User.objects.create_superuser(email=email, username=username, password=password)
+            password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+            admin = User.objects.create_superuser(
+                email=email, 
+                username=username, 
+                password=password
+                )
             admin.is_active = True
             admin.is_admin = True
             admin.save()
         else:
-            print('Admin accounts can only be initialized if no Users exist')
+            print(
+                'Admin accounts can only be initialized if no Users exist'
+                )
