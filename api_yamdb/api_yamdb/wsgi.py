4b70cafce03c2093dfb183b189406f0f1bfd8de3
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 13:58:29 +0300

    test wsgi

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -2,17 +2,15 @@ import os
 
 from users.models import User
 from django.core.wsgi import get_wsgi_application
-from django.shortcuts import get_object_or_404
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 application = get_wsgi_application()
 
-users = get_object_or_404(
-    User, username=os.getenv('DJANGO_SUPERUSER_USERNAME')
-)
 
-if not users:
+try:
+    users = User.objects.get(username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
+except User.DoesNotExist:
     User.objects.create_superuser(
         username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
         email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
