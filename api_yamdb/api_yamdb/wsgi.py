164783eadca820171f0b746934738c1a5522427c
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 14:05:56 +0300

    test wsgi

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -8,13 +8,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 application = get_wsgi_application()
 
 
-try:
-    users = User.objects.get(username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
-except User.DoesNotExist:
-    User.objects.create_superuser(
-        username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
-        email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
-        password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
-        is_active=True,
-        is_staff=True
-    )
+# try:
+#     users = User.objects.get(username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
+# except User.DoesNotExist:
+#     User.objects.create_superuser(
+#         username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+#         email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+#         password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
+#         is_active=True,
+#         is_staff=True
+#     )
