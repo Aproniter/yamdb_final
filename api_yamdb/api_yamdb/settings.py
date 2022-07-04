112663ca5d7c3c42792b420587f6ee874d9e2a97
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 12:46:03 +0300

    test django createsuperuser

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -33,8 +33,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'django_filters',
-    'corsheaders',
-    'django_createsuperuserwithpassword'
+    'corsheaders',    
 ]
 
 AUTH_USER_MODEL = 'users.User'
--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -1,7 +1,19 @@
 import os
 
+from users.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 application = get_wsgi_application()
+
+users = get_object_or_404(User, username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
+
+if not users:
+    User.objects.create_superuser(
+        username=os.getenv('DJANGO_SUPERUSER_USERNAME'), 
+        email=os.getenv('DJANGO_SUPERUSER_EMAIL'), 
+        password=os.getenv('DJANGO_SUPERUSER_PASSWORD'), 
+        is_active=True, 
+        is_staff=True
+    )
