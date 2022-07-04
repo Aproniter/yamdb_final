77368a9726db118ee15fd97454707bce250c57e9
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 14:12:29 +0300

    test wsgi

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -1,20 +1,7 @@
 import os
 
-from users.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 application = get_wsgi_application()
-
-
-# try:
-#     users = User.objects.get(username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
-# except User.DoesNotExist:
-#     User.objects.create_superuser(
-#         username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
-#         email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
-#         password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
-#         is_active=True,
-#         is_staff=True
-#     )
