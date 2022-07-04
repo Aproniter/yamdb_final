e8b98ee54fb2e35a6612639b985572a9896e0581
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 14:22:17 +0300

    add initadmin

---
+++ api_yamdb/users/management/commands/initadmin.py
@@ -0,0 +1,19 @@
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from users.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:           
+            username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+            email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+            password = os.getenv('DJANGO_SUPERUSER_PASSWORD')            
+            admin = Account.objects.create_superuser(email=email, username=username, password=password)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
