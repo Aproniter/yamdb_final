98a2ec4f7d2e4d410b7628cedf2682c864557483
Aproniter <ivan.belokrylov@gmail.com>
2022-07-04 12:48:50 +0300

    test django createsuperuser

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -2,18 +2,21 @@ import os
 
 from users.models import User
 from django.core.wsgi import get_wsgi_application
+from django.shortcuts import get_object_or_404
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 application = get_wsgi_application()
 
-users = get_object_or_404(User, username=os.getenv('DJANGO_SUPERUSER_USERNAME'))
+users = get_object_or_404(
+    User, username=os.getenv('DJANGO_SUPERUSER_USERNAME')
+    )
 
 if not users:
     User.objects.create_superuser(
-        username=os.getenv('DJANGO_SUPERUSER_USERNAME'), 
-        email=os.getenv('DJANGO_SUPERUSER_EMAIL'), 
-        password=os.getenv('DJANGO_SUPERUSER_PASSWORD'), 
-        is_active=True, 
+        username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+        email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+        password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
+        is_active=True,
         is_staff=True
     )
